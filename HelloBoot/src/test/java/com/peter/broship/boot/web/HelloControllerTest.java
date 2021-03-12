00198152b6d10d2c51d83334c24949ed8d4d12d1
peter <gudqo1995@naver.com>
2021-03-13 00:42:31 +0900

    test

--- HelloBoot/src/test/java/com/peter/broship/boot/web/HelloControllerTest.java
+++ HelloBoot/src/test/java/com/peter/broship/boot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.peter.broship.boot.web;
 
+import com.peter.broship.boot.config.auth.SecurityConfig;
 import com.peter.broship.boot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,13 +18,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception{
         String hello = "hello~";//다르게 적으면 테스트 실패
 
@@ -30,6 +38,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception{
         String name = "good";
         int amount = 999;
